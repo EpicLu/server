370bffd26fd0e38e990622c9e91560f0d9e7c13e
EpicLu <2219229705@qq.com>
2023-04-10 01:02:09 +0800

    针对云服务器改动的cserver

---
+++ cserver/CMakeFiles/event.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/event.dir/src/event_send_data.o"
+  "CMakeFiles/event.dir/src/event_add.o"
+  "CMakeFiles/event.dir/src/event_recv_data.o"
+  "CMakeFiles/event.dir/src/event_del.o"
+  "CMakeFiles/event.dir/src/event_init.o"
+  "CMakeFiles/event.dir/src/event_set.o"
+  "CMakeFiles/event.dir/src/event_accept.o"
+  "libevent.pdb"
+  "lib/libevent.so"
+  "lib/libevent.so.1.2"
+  "lib/libevent.so.1"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/event.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
--- cserver/src/http_send_file.c
+++ cserver/src/http_send_file.c
@@ -6,6 +6,7 @@ void http_send_file(int cfd, void *arg)
     char *file = (char *)arg;
     char buf[BUFSIZ];
     int ret;
+    int len;
 
     printf("send file: %s\n", file);
 
@@ -15,11 +16,12 @@ void http_send_file(int cfd, void *arg)
         perror("file open error ");
     }
 
-    while ((ret = read(fd, buf, sizeof(buf))) != 0)
+    while ((len = read(fd, buf, sizeof(buf))) != 0)
     {
+        printf("read file: %s bufsize:%d\n", file, len);
     tryagain:
-        ret = send(cfd, buf, ret, 0);
-        printf("file: %s bufsize:%d\n", file, ret);
+        ret = send(cfd, buf, len, 0);
+        printf("send file: %s bufsize:%d\n", file, ret);
         if (ret == -1)
         {
             if (ret == EAGAIN || ret == EINTR) // 阻塞状态不算发送错误
@@ -31,8 +33,10 @@ void http_send_file(int cfd, void *arg)
             }
         }
     }
-
-    printf("send file: %s success\n", file);
+    if (ret != -1)
+        printf("send file: %s success\n", file);
+    else
+        printf("send file: %s failed errno = %d\n", file, errno);
 
     // 发送完关闭
     close(fd);
