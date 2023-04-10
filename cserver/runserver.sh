2daefe1e5b1144cae06f0e0bbb53a4c176f0cde8
EpicLu <2219229705@qq.com>
2023-04-11 00:36:06 +0800

    修复web2的BUG

---
+++ cserver/runserver.sh
@@ -0,0 +1,13 @@
+while true
+do
+    ps -ef | grep "./bin/server 1206 ../web2/docs" | grep -v "grep"
+    if [ "$?" -eq 1 ]
+        then
+        ./bin/server 1206 ../web2/docs #启动应用，修改成自己的启动应用脚本或命令
+	sh ./runserver.sh
+        echo "server has been restarted!"
+    else
+        echo "server already started!"
+    fi
+    sleep 10
+done
