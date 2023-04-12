8b2f9ecf269265387b9d76b36ba54316c72a1a62
EpicLu <2219229705@qq.com>
2023-04-12 23:43:12 +0800

    删除post页面的js

--- cserver/runserver.sh
+++ cserver/runserver.sh
@@ -3,8 +3,7 @@ do
     ps -ef | grep "./bin/server 1206 ../web2/docs" | grep -v "grep"
     if [ "$?" -eq 1 ]
         then
-        ./bin/server 1206 ../web2/docs #启动应用，修改成自己的启动应用脚本或命令
-	sh ./runserver.sh
+        ./bin/server 80 ../web2/docs #启动应用，修改成自己的启动应用脚本或命令
         echo "server has been restarted!"
     else
         echo "server already started!"
